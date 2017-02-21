fc97a80cd2025b2d38fd6d156a3389f70431a10b
Kwame Porter Robinson <kporterrobinson@gmail.com>
2017-02-21 12:55:31 -0500

    directly adding the predict file
    due to to local side git snafus

--- predict_diff_in_hours.py
+++ predict_diff_in_hours.py
@@ -53,23 +53,30 @@ def cross_validate_me(clf,
     print("%d-fold Cross Validation Accuracy: %0.2f (+/- %0.2f)" % (len(scores), scores.mean(), scores.std() * 2))
 
     # same as accuracy
-    scores = cross_val_score(clf,
-                             design_matrix[predicator_variables],
-                             np.ravel(design_matrix[response_variable].values),
-                             cv=make_cv(test_size, random_seed),
-                             n_jobs=2,
-                             scoring=make_scorer(r2_score))
+    #scores = cross_val_score(clf,
+    #                         design_matrix[predicator_variables],
+    #                         np.ravel(design_matrix[response_variable].values),
+    #                         cv=make_cv(test_size, random_seed),
+    #                         n_jobs=2,
+    #                         scoring=make_scorer(r2_score))
 
-    print("(%d-fold average) R^2: %0.2f (+/- %0.2f)" % (len(scores), scores.mean(), scores.std() * 2))
+    #print("(%d-fold average) R^2: %0.2f (+/- %0.2f)" % (len(scores), scores.mean(), scores.std() * 2))
 
-    print("Predictor Variables Used: (response variable {})".format(response_variable[0]),
-          "\n\t",
-          "\n\t".join(predicator_variables))
+    #print("Predictor Variables Used: (response variable {})".format(response_variable[0]),
+    #      "\n\t",
+    #      "\n\t".join(predicator_variables))
     print("\t-----------------------------")
 
 # Variables
 random_seed = 0
 test_size = 0.10
+
+orig = {'filename':"./design_matrix/design_matrix_task_model_bing.csv",
+        'predicator_variables':['Data Value1', 'social_index', 'pm_index',
+                                'relevance', 'importance', 'job_zone',
+                                'log_bing'],
+        'response_variable' : ["difference in hours"],
+        'sep':'\t'}
 
 feb5 = {'filename':"task_model_data.feb5.bsv",
         'predicator_variables':["importance", "relevance", "task_person_hours1",
@@ -80,21 +87,21 @@ feb5 = {'filename':"task_model_data.feb5.bsv",
         'sep':"|"}
 
 feb18 = {'filename':"task_model_training_data.feb18.bsv",
-         'predicator_variables':["importance", "relevance", "task_person_hours1",
-                                "year_span", "pm_job", "social_job",
-                                "social_job", "normalized_job_zone", "pmjob_x_jobzone",
-                                "social_x_jobzone"],
+         'predicator_variables':["importance", "relevance", "normalized_job_zone",
+                                 "year_span", "social_job", "creative_job",
+                                 "pm_job", "automation_index"],
          'response_variable' : ["difference_in_hours"],
          'sep':'|'}
 
-orig = {'filename':"./design_matrix/design_matrix_task_model_bing.csv",
-        'predicator_variables':['Data Value1', 'social_index', 'pm_index',
-                                'relevance', 'importance', 'job_zone',
-                                'log_bing'],
-        'response_variable' : ["difference in hours"],
-        'sep':'\t'}
+predictme = {'filename':"task_forecast_full_data.bsv",
+             'predicator_variables':["importance", "relevance", "normalized_job_zone",
+                                     "year_span", "social_job", "creative_job",
+                                     "pm_job", "automation_index"],
+             'response_variable' : ["difference_in_hours"],
+             'sep':'|'}
 
-dataset_info = [feb5, feb18, orig]
+dataset_info = [feb18] #[feb5, feb18, orig]
+dataset_predict = [] # [predictme]
 
 # Driver loop (runs data, classifers over another, measuring accuracy, etc)
 for info in dataset_info:  # cross validate set of regressors over each dataset
@@ -108,13 +115,14 @@ for info in dataset_info:  # cross validate set of regressors over each dataset
     regressors = [\
         RandomForestRegressor(n_estimators=10,
                               random_state=random_seed,
-                              n_jobs=2),
-        SGDRegressor()]
-
-
-
-        #linear_model.LinearRegression(normalize=False,
-        #                              n_jobs=1)]
+                              criterion="mae",
+                              max_depth=None,
+                              oob_score=True,
+                              n_jobs=4)]
+    #regressors = [\
+    #    RandomForestRegressor(n_estimators=10,
+    #                          random_state=random_seed,
+    #                          n_jobs=2)]
 
     for regressor in regressors:
         cross_validate_me(regressor,
@@ -124,3 +132,20 @@ for info in dataset_info:  # cross validate set of regressors over each dataset
                           random_seed = random_seed,
                           predicator_variables = predicator_variables,
                           response_variable = response_variable)
+
+        # Output predictions, accuracy should be somethign like what we see in
+        # cross validate
+        print("\n training on entire ", filename)
+        trained = regressor.fit(design_matrix[predicator_variables],
+                                np.ravel(design_matrix[response_variable].values))
+        for predict in dataset_predict:
+            filename = predict['filename']
+            predicator_variables = predict['predicator_variables']
+            response_variable = predict['response_variable']
+            sep = predict['sep']
+
+            print("\n predicting on ", filename)
+            predict_matrix = pd.read_csv(filename, sep=sep)[predicator_variables]
+            predict_matrix[response_variable[0]] = trained.predict(predict_matrix[predicator_variables])
+
+            predict_matrix.to_csv(filename+".predict", sep=sep)
